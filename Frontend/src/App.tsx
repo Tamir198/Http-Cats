804723829375f298a74cf0928cbdc48b92bd5c57
Tamir <1tamir198@gmail.com>
2021-11-17 20:19:31 +0200

    Reset project to front with ts
    Reset the project and files to front end with ts

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Card } from './Components/Card';
+
+function App() {
+  return (
+    <div className="App">
+        <Card title="Hey!" paragraph="I am typescript Componenet" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Components/Card.tsx
@@ -0,0 +1,12 @@
+type CardProps = {
+  title: string,
+  paragraph: string
+}
+
+export const Card = ( { title, paragraph }: CardProps) => <aside>
+  <h2>{title}</h2>
+  <p>
+    {paragraph}
+  </p>
+</aside>
+
