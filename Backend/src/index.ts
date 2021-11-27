3ebee5a6e0a8fcd8d8cb1f2ce5ed6ba6b960b57c
Tamir <1tamir198@gmail.com>
2021-11-27 12:53:38 +0200

    Adding mock data

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,10 +2,16 @@ import express from 'express'
 const app = express()
 const port = 5000
 
+var Data = require('./DB/mockData');
+
 let test:string = "Hey from server using ts";
 
 app.get('/', (_, res) => {
-  res.status(200).send(test)
+  res.status(200).send(Data)
+})
+
+app.get('/:statusCode', (req, res) => {
+  res.status(200).send(Data[req.params.statusCode])
 })  
 
 app.listen(port, () => console.log(`Running on port ${port}`))
