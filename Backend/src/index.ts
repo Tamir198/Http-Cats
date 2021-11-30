e6973a64bde1e9ee98d63935f927a2ec11bc0d96
Tamir <1tamir198@gmail.com>
2021-11-30 15:59:57 +0200

    Small code  arrangement

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,20 +1,19 @@
 import express from 'express'
+const cors = require('cors')
 
-var cors = require('cors')
+const { port } = require('./config.json');
+var data = require('./DB/mockData');
+
 const app = express()
 app.use(cors());
 
-const port = 8888
-
-var Data = require('./DB/mockData');
-let test:string = "Hey from server using ts";
 
 app.get('/allData', (_, res) => {
-  res.status(200).send(Data)
+  res.status(200).send(data);
 })
 
 app.get('/:statusCode', (req, res) => {
-  res.status(200).send(Data[req.params.statusCode])
+  res.status(200).send(data[req.params.statusCode]);
 })  
 
 app.listen(port, () => console.log(`Running on port ${port}`))
