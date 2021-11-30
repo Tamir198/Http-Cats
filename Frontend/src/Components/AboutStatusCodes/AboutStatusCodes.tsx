8c639b8430c30a4d3edf8cf5ae1bb567d6d62bfd
Tamir <1tamir198@gmail.com>
2021-11-30 14:58:29 +0200

    Finish website (not mobile yet) design and UI

--- Frontend/src/Components/AboutStatusCodes/AboutStatusCodes.tsx
+++ Frontend/src/Components/AboutStatusCodes/AboutStatusCodes.tsx
@@ -4,9 +4,9 @@ import { StatusCodesTypes } from './StatusCodesTypes';
 
 export const AboutStatusCodes = () => {
   return <div className="about-status-container">
-    <h4>What are status codes:</h4>
+    <h2>What are status codes:</h2>
     <p>HTTP response status codes indicate whether a specific HTTP request has been successfully completed.</p>
-    <p>Responses are grouped in five classes:</p>
+    <h3>Responses are grouped in five classes:</h3>
     <StatusCodesTypes></StatusCodesTypes>
   </div>
 }
